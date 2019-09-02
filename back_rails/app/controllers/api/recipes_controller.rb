cde59498fe7707cebc3adaef40670f73f3c2b6c8
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-09-01 23:33:40 -0400

    created api directory and /new route

---
+++ back_rails/app/controllers/api/recipes_controller.rb
@@ -0,0 +1,21 @@
+class Api::RecipesController < ApplicationController
+  def index
+    # puts query_parameters.inspect
+    recipes = BigOvenService.get_steps
+    render json: recipes
+  end
+
+  # user recipe form
+  def new
+    recipes = BigOvenService.search_recipes
+    render json: recipes
+
+    # something like the below when steps DB is seeded
+    # raise @steps.inspect # debug
+    # @steps = Step.all
+  end
+
+
+
+end
+
--- back_rails/app/controllers/recipes_controller.rb
+++
@@ -1,9 +0,0 @@
-class RecipesController < ApplicationController
-  def index
-    # puts query_parameters.inspect
-    recipes = BigOvenService.get_steps
-    render json: recipes
-  end
-
-end
-
