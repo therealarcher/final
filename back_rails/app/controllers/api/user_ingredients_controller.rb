1cf65c299691abf4f98c9c3e0958dbe686c5d317
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-09-09 11:53:59 -0400

    fixed ingredient required count bug

--- back_rails/app/controllers/api/user_ingredients_controller.rb
+++ back_rails/app/controllers/api/user_ingredients_controller.rb
@@ -16,12 +16,14 @@ class Api::UserIngredientsController < ApplicationController
 
   def index
     tot_ingr_required = Recipe.find(params[:id]).ingredients
-    tot_num_ingr_required = tot_ingr_required.count
     
     tot_ingr_req_array = []
     tot_ingr_required.each do |i|
       tot_ingr_req_array.push(i.name)
     end
+    
+    #total number of ingredients required for recipe
+    tot_num_ingr_required = tot_ingr_req_array.count
 
     # render json: tot_ingr_req_array
 
@@ -30,18 +32,19 @@ class Api::UserIngredientsController < ApplicationController
     current_user.user_ingredients.each do |ingredient|
       keyword = "%#{ingredient.name}%"
       q = tot_ingr_required.where("LOWER(ingredients.name) LIKE ?", keyword)
+      
       q.each do |entry|
         matched_ingr.push(entry.name)
       end
     end
     # render json: matched_ingr
 
-    ingr_to_buy_arr = tot_ingr_req_array - matched_ingr - $queried_ingredients # and still need to subtract original queried items
-    render json: ingr_to_buy_arr
+    ingr_to_buy_arr = tot_ingr_req_array - matched_ingr #- $queried_ingredients  #and still need to subtract original queried items
+    render json: ingr_to_buy_arr #additional ingredients user needs to make recipe
 
     ## sort ascending for recipes when user logged in
     @@user_num_ingr_required = tot_num_ingr_required - matched_ingr.count
-    # render json: @@user_num_ingr_required
+    # render json: @@user_num_ingr_required #quantity of ingredients user needs to make recipe
 
 
     # just an array of similar terms with ingredients table
