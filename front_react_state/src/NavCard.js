7be141a7a2d886ffcd9114f5959b86d2683b4259
Simon Garber <Simon.garber@gmail.com>
2019-09-11 17:37:00 -0400

    Added hide saved recipes toggle

--- front_react_state/src/NavCard.js
+++ front_react_state/src/NavCard.js
@@ -7,7 +7,7 @@ import IngredientModal from './components/IngredientModal';
 import Navbar from 'react-bootstrap/Navbar';
 import './styles/Card.css';
 import NewUser from './components/NewUser';
-
+import Toggle from './utilities/Toggle';
 class NavCard extends Component {
   constructor() {
     super();
@@ -90,85 +90,93 @@ class NavCard extends Component {
   render() {
     return (
       <div>
-        <Fragment>
-          <Navbar bg="light" expand="lg">
-            {/* <Navbar.Brand href="#home">RecipEasy</Navbar.Brand> */}
-            <h1 class="main-title">recipEasy</h1>
-            <Fragment>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto"></Nav>
-
-                {this.props.currentUser ? (
-                  <Fragment>
-                    <h4>Logged in as : {this.props.currentUser}</h4>
-                    <button onClick={this.props.handleLogout}>Logout</button>
-                  </Fragment>
-                ) : (
-                  <Fragment>
-                    <NewUser
-                      HandleUpdate={this.props.HandleUpdate}
-                      updateCurrentUser={this.props.updateCurrentUser}
-                      name={this.props.name}
-                    />
-                  </Fragment>
-                )}
-              </Navbar.Collapse>
-            </Fragment>
-          </Navbar>
-
+        <Navbar bg="light" expand="lg">
+          {/* <Navbar.Brand href="#home">RecipEasy</Navbar.Brand> */}
+          <h1 class="main-title">recipEasy</h1>
           <Fragment>
-            <div>
-              <Card key={uuidv4} className="Card-container">
-                <Card.Body>
-                  <Card.Title></Card.Title>
-                  <Button>Add items to pantry</Button>
-                  <form onSubmit={this.handleSubmit}>
-                    <input
-                      key={uuidv4}
-                      value={this.state.name}
-                      type="text"
-                      onChange={this.handleChange}
-                    />
-                  </form>
-                  <Fragment>
-                    <Button onClick={this.getSavedIngredients}>
-                      Display Pantry
-                    </Button>
-                  </Fragment>
-                  <Fragment>
-                    <Button onClick={this.getSavedRecipes} type="submit">
-                      Show Saved Recipes
-                    </Button>
-                  </Fragment>
-                  <Fragment>
-                    <IngredientModal
-                      hide={() => this.setState({ showIngredientModal: false })}
-                      show={this.state.showIngredientModal}
-                      savedIngredients={this.state.savedIngredients}
-                    />
-                  </Fragment>
-                </Card.Body>
-                <Button type="submit"></Button>
-              </Card>
-            </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            <div>Saved Recipes Here</div>
-            <Row>
-              {this.state.savedRecipes.map((savedRecipe) => {
-                return (
-                  <RecipeView
-                    key={savedRecipe.id}
-                    id={savedRecipe.id}
-                    name={savedRecipe.name}
-                    image={savedRecipe.image}
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto"></Nav>
+
+              {this.props.currentUser ? (
+                <Fragment>
+                  <h4>Logged in as : {this.props.currentUser}</h4>
+                  <button onClick={this.props.handleLogout}>Logout</button>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <NewUser
+                    HandleUpdate={this.props.HandleUpdate}
+                    updateCurrentUser={this.props.updateCurrentUser}
+                    name={this.props.name}
                   />
-                );
-              })}
-            </Row>
+                </Fragment>
+              )}
+            </Navbar.Collapse>
           </Fragment>
-        </Fragment>
+        </Navbar>
+
+        <div>
+          <Card key={uuidv4} className="Card-container">
+            <Card.Body>
+              <Card.Title></Card.Title>
+              <Button>Add items to pantry</Button>
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  key={uuidv4}
+                  value={this.state.name}
+                  type="text"
+                  onChange={this.handleChange}
+                />
+              </form>
+              <Fragment>
+                <Button onClick={this.getSavedIngredients}>
+                  Display Pantry
+                </Button>
+              </Fragment>
+              <Fragment>
+                <Button onClick={this.getSavedRecipes} type="submit">
+                  Update Saved Recipes
+                </Button>
+              </Fragment>
+              <Fragment>
+                <IngredientModal
+                  hide={() => this.setState({ showIngredientModal: false })}
+                  show={this.state.showIngredientModal}
+                  savedIngredients={this.state.savedIngredients}
+                />
+              </Fragment>
+            </Card.Body>
+            <Button type="submit"></Button>
+          </Card>
+        </div>
+
+        <Toggle
+          render={({ on, toggle }) => (
+            <div>
+              {on && (
+                <div>
+                  <Row>
+                    {this.state.savedRecipes.map((savedRecipe) => {
+                      return (
+                        <Fragment>
+                          <RecipeView
+                            key={savedRecipe.id}
+                            id={savedRecipe.id}
+                            name={savedRecipe.name}
+                            image={savedRecipe.image}
+                          />
+                        </Fragment>
+                      );
+                    })}
+                  </Row>
+                </div>
+              )}
+              <button onClick={toggle}>Hide Saved Recipes</button>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- front_react_state/src/utilities/Toggle.js
+++ front_react_state/src/utilities/Toggle.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import React from 'react';
 export default class Toggle extends Component {
   state = { on: false };
   toggle = () => {
@@ -8,10 +8,14 @@ export default class Toggle extends Component {
     });
   };
   render() {
-    const { children } = this.props;
-    return children({
-      on: this.state.on,
-      toggle: this.toggle
-    });
+    const { render } = this.props;
+    return (
+      <div>
+        {render({
+          on: this.state.on,
+          toggle: this.toggle
+        })}
+      </div>
+    );
   }
 }
